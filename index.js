fc302a20fd5293cbba2952bad852c285c96811ad
Kevin Cheung <kevinchikitcheung@gmail.com>
2018-05-30 14:51:58 -0500

    complete node-auth-afternoon

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const students = require('./students.json');
 
 const app = express();
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, { clientID: user.id, email: user._json.email, name: user._json.name });
+})
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+})
+
+passport.use(new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/login',
+    scope: "openid email profile"
+},
+function(accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+}
+));
+
+app.get('/login', passport.authenticate('auth0', {
+    successRedirect: '/students',
+    failureRedirect: '/login',
+    connection: 'github'
+}));
+
+function authenticated(req, res, next){
+    if (req.user){
+        next();
+    }else{
+    res.status(401).json();
+    }
+};
+
+app.get('/students', authenticated, (req, res, next) => {
+    res.status(200).json(students);
+});
+
+
+
 
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen(port, () => { console.log(`${port} I READ YOU`); });
